3e8e7631df90dda0905d3082780685a5a2fb53fb
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-10 08:21:19 +0200

    kk: Style theme component

--- src/app/theme/theme.component.ts
+++ src/app/theme/theme.component.ts
@@ -2,7 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-theme',
-  templateUrl: './theme.component.html'
+  templateUrl: './theme.component.html',
+  styles: [`
+    h1 {
+      margin-top: 25px;
+    }
+    .top-margin {
+        margin-top: 20px;
+    }
+  `]
 })
 export class ThemeComponent implements OnInit {
 
