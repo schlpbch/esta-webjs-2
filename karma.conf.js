e67d465f7e33c199d5c542bb48c00d87dc23f9f6
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-19 08:34:50 +0200

    kk: Change prebuild script and start ng test with arguments

--- karma.conf.js
+++ karma.conf.js
@@ -5,73 +5,69 @@ var ip = require('ip');
 
 module.exports = function (config) {
 
-  var seleniumWebgrid = {
-    hostname: 'webtestgrid.sbb.ch',
-    port: 4444
-  };
+    var seleniumWebgrid = {
+        hostname: 'webtestgrid.sbb.ch',
+        port: 4444
+    };
 
-  config.set({
-    hostname: process.env.host || ip.address(),
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('karma-webdriver-launcher'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    files: [
-      {pattern: './src/test.ts', watched: false}
-    ],
-    preprocessors: {
-      './src/test.ts': ['@angular/cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts', 'tsx']
-    },
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      config: './angular-cli.json',
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
-      : ['progress', 'kjhtml'],
-    port: process.env.externalport || 7777,
-    colors: true,
-    logLevel: config.LOG_DEBUG,
-    autoWatch: false,
-    customLaunchers: {
-      'SeleniumCH': {
-        base: 'WebDriver',
-        config: seleniumWebgrid,
-        browserName: 'chrome'
-      },
-      'SeleniumFF': {
-        base: 'WebDriver',
-        config: seleniumWebgrid,
-        browserName: 'firefox'
-      },
-      'SeleniumIE': {
-        base: 'WebDriver',
-        config: seleniumWebgrid,
-        browserName: 'internet explorer',
-        'x-ua-compatible': 'IE=edge'
-      }
-    },
-    browsers: ['SeleniumCH'],
-    singleRun: false,
-    captureTimeout: 60 * 1000,
-    browserDisconnectTimeout: 15 * 1000,
-    browserDisconnectTolerance: 5,
-    browserNoActivityTimeout: 15 * 10000
-  });
+    config.set({
+        hostname: process.env.host || ip.address(),
+        basePath: __dirname,
+        frameworks: ['jasmine', '@angular/cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('karma-webdriver-launcher'),
+            require('@angular/cli/plugins/karma')
+        ],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        files: [
+            {pattern: './src/test.ts', watched: false}
+        ],
+        preprocessors: {
+            './src/test.ts': ['@angular/cli']
+        },
+        mime: {
+            'text/x-typescript': ['ts', 'tsx']
+        },
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly'],
+            fixWebpackSourcePaths: true
+        },
+        angularCli: {
+            config: './angular-cli.json',
+            environment: 'dev'
+        },
+        reporters: config.angularCli && config.angularCli.codeCoverage
+            ? ['progress', 'coverage-istanbul']
+            : ['progress', 'kjhtml'],
+        port: process.env.externalport || 7777,
+        colors: true,
+        logLevel: config.LOG_DEBUG,
+        autoWatch: false,
+        customLaunchers: {
+            'SeleniumCH': {
+                base: 'WebDriver',
+                config: seleniumWebgrid,
+                browserName: 'chrome'
+            },
+            'SeleniumFF': {
+                base: 'WebDriver',
+                config: seleniumWebgrid,
+                browserName: 'firefox'
+            },
+            'SeleniumIE': {
+                base: 'WebDriver',
+                config: seleniumWebgrid,
+                browserName: 'internet explorer',
+                'x-ua-compatible': 'IE=edge'
+            }
+        },
+        browsers: ['Chrome'],
+        singleRun: true
+    });
 };
