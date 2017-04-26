fb08ebcf418ff7af04a95ec05769e16d8638385d
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-26 18:04:19 +0200

    kk: About Tests added

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -7,6 +7,8 @@ import {TranslateService} from "@ngx-translate/core";
 import {XHRBackend, HttpModule} from "@angular/http";
 import {MockBackend} from "@angular/http/testing";
 import {NotificationsService} from "angular2-notifications";
+import {PostsService} from '../shared/posts.service';
+import {Observable} from 'rxjs';
 
 @Pipe({name: 'translate'})
 class MockPipe implements PipeTransform {
@@ -19,8 +21,33 @@ describe('AboutComponent', () => {
     let component: AboutComponent;
     let fixture: ComponentFixture<AboutComponent>;
 
-    class NotificationServiceMock{}
-    class TranslateServiceMock{}
+    class NotificationServiceMock {
+    }
+    class TranslateServiceMock {
+    }
+
+    class MockPostsService {
+        public getAllPosts() {
+            return Observable.of([
+                {
+                    id: 26,
+                    title: 'hi 1',
+                },
+                {
+                    id: 27,
+                    title: 'hi 2',
+                }
+            ]);
+        }
+
+        public getPostById(id) {
+            return Observable.of(
+                {
+                    id: id,
+                    title: 'hi 4',
+                });
+        }
+    }
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -30,7 +57,15 @@ describe('AboutComponent', () => {
                 {provide: NotificationsService, useClass: NotificationServiceMock},
                 {provide: TranslateService, useValue: TranslateServiceMock}
             ]
-        }).compileComponents();
+        })
+            .overrideComponent(AboutComponent, {
+                set: {
+                    providers: [
+                        {provide: PostsService, useClass: MockPostsService}
+                    ]
+                }
+            })
+            .compileComponents();
     }));
 
     beforeEach(() => {
@@ -41,7 +76,19 @@ describe('AboutComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('has a name property [aboutMessage]', () => {
+        expect(component.aboutMessage).toBe('Über dieses Template');
+    });
+
+    it('onInit should subscribe to PostsService [getAllPosts]', () => {
+        component.ngOnInit();
+        expect(component.posts.length).toBe(2);
+        expect(component.posts[0].title).toBe('hi 1');
+        expect(component.posts[1].title).toBe('hi 2');
+    });
+
+    it('onInit should subscribe to PostsService [getPostById]', () => {
+        component.ngOnInit();
+        expect(component.postById.title).toBe('hi 4');
     });
 });
