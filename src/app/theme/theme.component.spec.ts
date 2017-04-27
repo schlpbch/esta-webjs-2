cdcb611635d7ee9072470807210baf13eab59fe6
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-27 07:51:22 +0200

    kk: Extend theme component test

--- src/app/theme/theme.component.spec.ts
+++ src/app/theme/theme.component.spec.ts
@@ -1,4 +1,12 @@
-/* tslint:disable:no-unused-variable */
+/**
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2017.
+ *
+ * ESTA WebJS: Unit-Tests ThemeComponent
+ *
+ * @author u218609 (Kevin Kreuzer)
+ * @version: 2.0.0
+ * @since 27.04.2017, 2017.
+ */
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ThemeComponent} from './theme.component';
 
@@ -18,7 +26,7 @@ describe('ThemeComponent', () => {
         fixture.detectChanges();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should have a property with the bootstrap docu Url', () => {
+        expect(component.bootstrapDocumentationUrl).toBe('https://v4-alpha.getbootstrap.com/components');
     });
 });
