1f4dc7d261bbed4413c44a8a8fbf538f6cbb2084
U218609 <kevin.kreuzer@sbb.ch>
2017-05-01 17:36:02 +0200

    kk: Add multiple capabilities property

--- protractor.webgrid.conf.js
+++ protractor.webgrid.conf.js
@@ -1,34 +1,46 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {SpecReporter} = require('jasmine-spec-reporter');
 const ip = require('ip');
 
 let hostname = process.env.host || ip.address();
 let externalport = process.env.externalport || 4200;
 
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  baseUrl: 'http://' + hostname + ':' + externalport,
-  seleniumAddress: 'http://webtestgrid.sbb.ch:4444/wd/hub',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  beforeLaunch: function() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-  },
-  onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
-};
+    allScriptsTimeout: 11000,
+    specs: [
+        './e2e/**/*.e2e-spec.ts'
+    ],
+    multiCapabilities: [
+        {
+            browserName: 'chrome',
+            seleniumAddress: hostname + ':' + externalport
+        },
+        {
+            browserName: 'firefox',
+            seleniumAddress: hostname + ':' + externalport
+        }
+    ],
+    baseUrl: 'http://' + hostname + ':' + externalport,
+    seleniumAddress: 'http://webtestgrid.sbb.ch:4444/wd/hub',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function () {
+        }
+    }
+    ,
+    beforeLaunch: function () {
+        require('ts-node').register({
+            project: 'e2e/tsconfig.e2e.json'
+        });
+    }
+    ,
+    onPrepare()
+    {
+        jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+    }
+}
+;
