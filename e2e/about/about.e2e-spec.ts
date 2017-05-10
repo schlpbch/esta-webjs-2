92bd8e1e0932f42bd9e6fe009934ed041af4c333
u220374 <reto.lehmann@sbb.ch>
2017-05-10 14:58:18 +0200

    E2E on selenium grid

--- e2e/about/about.e2e-spec.ts
+++ e2e/about/about.e2e-spec.ts
@@ -14,6 +14,7 @@ describe('AboutPage', function () {
     let aboutPage: AboutPage;
 
     beforeEach(() => {
+      console.log('reto2', browser.baseUrl);
         aboutPage = new AboutPage();
         browser.get('#/about');
     });
